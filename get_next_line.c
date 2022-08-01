96fd8ef0fe56e4bc38a6b29988dc17d320c36863
Junseo Lee <junlee2@c1r4s6.42seoul.kr>
2022-08-01 16:15:23 +0900

    Re making codes : using linked list

--- get_next_line.c
+++ get_next_line.c
@@ -6,51 +6,107 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 09:51:24 by junlee2           #+#    #+#             */
-/*   Updated: 2022/07/29 11:08:52 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2022/08/01 16:13:05 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdlib.h>
 #include "get_next_line.h"
-#include <stddef.h>
 
 #include <fcntl.h>
-#include <stdio.h>
 #include "ft_printf.h"
 
-size_t	ft_strlen(const char *str)
+void	ft_lstclear(t_strlist *lst)
 {
-	size_t	returni;
+	t_strlist	*temp;
 
-	returni = 0;
-	while (str[returni])
-		returni++;
-	return (returni);
+	while (lst)
+	{
+		temp = lst->next;
+		free(lst);
+		lst=temp;
+	}
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+t_fdlist	*findlist(int fd, t_fdlist **list)
 {
-	char	*returns;
+	t_fdlist	*temp;
+
+	temp = *list;
+	if (temp == 0)
+	{
+		*list = (t_fdlist *)malloc(sizeof(t_fdlist));
+		if (*list)
+			(*list)->fd = -1;
+		return (*list);
+	}
+	while (!temp->next)
+	{
+		if (temp->fd == fd)
+			return(temp);
+		temp = temp->next;
+	}
+	temp->next = (t_fdlist *)malloc(sizeof(t_fdlist));
+	if (*list)
+		(*list)->fd = -1;
+	return (temp->next);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
 	size_t	i;
 
-	returns = (char *)malloc(len + 1);
-	if (!returns)
+	if (dst == 0 && src == 0)
 		return (0);
 	i = 0;
-	while (s[start + i] && i < len)
+	while (i < n)
 	{
-		returns[i] = s[start + i];
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	returns[i] = 0;
-	return (returns);
+	return (dst);
+}
+
+int	readfd(int fd, t_fdlist *fdlist, size_t *status, t_strlist *strlist)
+{
+	*status = read(fd, fdlist->buffer, BUFFER_SIZE);
+	if (*status == 0 || *status == -1)
+		return (-1);
+	
+	fdlist->index = 0;
+	return (0);
+}
+
+char	*next_line(int fd, t_fdlist *fdlist)
+{
+	size_t	status;
+	t_strlist	*strlist;
+
+	if (fdlist->fd == -1)
+		if (readfd(fd, fdlist, &status, strlist))
+			return (-1);
+	while (1)
+	{
+		while (fdlist->buffer[fdlist->index] == '\n' && fdlist->index < BUFFER_SIZE)
+			fdlist->index++;
+		if (fdlist->index == BUFFER_SIZE)
+			if (readfd(fd, fdlist, &status, strlist))
+				return (-1);
+	}
+	
 }
 
 char	*get_next_line(int fd)
 {
-	char	buffer[BUFFER_SIZE + 1];
-	static char	*returns;
+	static t_fdlist	*fdlist = 0;
+	t_fdlist		*workbench;
+	
+	if (fd < 0)
+		return ((void *)0);
+	workbench = findlist(fd, fdlist);
+	if (!workbench)
+		return ((void *)0);
+	next_line(fd, workbench);
 }
 
 int	main(void)
--- get_next_line.h
+++ get_next_line.h
@@ -6,20 +6,28 @@
 /*   By: junlee2 <junlee2@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 09:58:22 by junlee2           #+#    #+#             */
-/*   Updated: 2022/07/28 14:56:17 by junlee2          ###   ########seoul.kr  */
+/*   Updated: 2022/08/01 16:13:07 by junlee2          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-typedef struct s_list
-{
-	char			*content;
-	struct s_dlist	*next;
-	struct s_dlist	*prev;
-}	t_list;
-
 # define BUFFER_SIZE 100
+
+typedef struct s_fdlist
+{
+	int				fd;
+	char			buffer[BUFFER_SIZE];
+	int				index;
+	struct s_fdlist	*next;
+}	t_fdlist;
+
+typedef struct s_strlist
+{
+	char				buffer[BUFFER_SIZE];
+	struct s_strlist	*next;
+}	t_strlist;
+
 
 #endif
